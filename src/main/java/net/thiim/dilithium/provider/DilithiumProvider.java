bb35908d0d5610b14efe67e222938f681d5be209
Adnan <adnan@example.com>
2026-07-01 16:06:00 +0600

    Modernize DilithiumProvider by removing deprecated AccessController

--- src/main/java/net/thiim/dilithium/provider/DilithiumProvider.java
+++ src/main/java/net/thiim/dilithium/provider/DilithiumProvider.java
@@ -1,6 +1,5 @@
 package net.thiim.dilithium.provider;
 
-import java.security.AccessController;
 import java.security.Provider;
 
 public class DilithiumProvider extends Provider {
@@ -8,33 +7,26 @@ public class DilithiumProvider extends Provider {
 	public DilithiumProvider() {
 		super("Dilithium Provider", "0.1", "For experimental use only");
 		
-		 AccessController.doPrivileged(new java.security.PrivilegedAction<Object>() {
-	            @Override
-	            public Object run() {
-	                /*
-	                     * Key(pair) Generator engines
-	                 */
-	                put("KeyPairGenerator.Dilithium",
-	                        "net.thiim.dilithium.provider.DilithiumKeyPairGenerator");
-	                put("Alg.Alias.KeyPairGenerator.Dilithium", "Dilithium");
+			/*
+		 * Key(pair) Generator engines
+		 */
+		put("KeyPairGenerator.Dilithium",
+				"net.thiim.dilithium.provider.DilithiumKeyPairGenerator");
+		put("Alg.Alias.KeyPairGenerator.Dilithium", "Dilithium");
 
-	                /*
-                     * Key factories
-	                 */
-	                put("KeyFactory.Dilithium",
-	                        "net.thiim.dilithium.provider.DilithiumKeyFactory");
-	                put("Alg.Alias.KeyFactory.Dilithium", "Dilithium");
-	                
-	                /*
-                     * Signature engines
-	                 */
-	                put("Signature.Dilithium",
-	                        "net.thiim.dilithium.provider.DilithiumSignature");
-	                put("Alg.Alias.Signature.Dilithium", "Dilithium");
-
-	                return null;
-	            }
-	        });
+		/*
+		 * Key factories
+		 */
+		put("KeyFactory.Dilithium",
+				"net.thiim.dilithium.provider.DilithiumKeyFactory");
+		put("Alg.Alias.KeyFactory.Dilithium", "Dilithium");
+		
+		/*
+		 * Signature engines
+		 */
+		put("Signature.Dilithium",
+				"net.thiim.dilithium.provider.DilithiumSignature");
+		put("Alg.Alias.Signature.Dilithium", "Dilithium");
 
 	}
 
